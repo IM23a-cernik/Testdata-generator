b3f6dec43a90059ae401054c6437d346998a9bc3
IM23a-cernik <cernik@bzz.ch>
2024-03-19 11:39:42 +0100

    Added Menu, Localization selector and made the data ASCII compatible. Added postcodes

--- Testdata generator.py
+++ Testdata generator.py
@@ -1,9 +1,36 @@
 from faker import Faker
-name = input('Namen auswählen\n')
-fake = Faker()
-id = 0
-amount = int(input('How many entries do you want?\n'))
-with open(f'{name}.txt', 'w') as file:
-    for i in range(0, amount):
-        file.write(f'{id + 1}, {fake.first_name()}, {fake.last_name()}\n')
-        id += 1
+def menu():
+    name = input('Dateinamen auswählen\n')
+    place = input('Wählen sie das Land aus:\nde, en_US, en_UK\n')
+    fake = Faker(place)
+    choice = input('Wählen Sie den Generator:\nPerson: P\n')
+    if choice == 'p' or 'P':
+        person(name, fake)
+def person(name, fake):
+    id = 0
+    amount = int(input('Wieviele Einträge wollen Sie?\n'))
+    with open(f'{name}.txt', 'w') as file:
+        for i in range(0, amount):
+            first_name = fake.first_name()
+            if 'ä' in first_name:
+                first_name = first_name.replace("ä", "ae")
+            elif 'ü' in first_name:
+                first_name = first_name.replace("ü", "ue")
+            elif 'ö' in first_name:
+                first_name = first_name.replace("ö", "oe")
+            elif 'ß' in first_name:
+                first_name = first_name.replace("ß", "ss")
+            last_name = fake.last_name()
+            if 'ä' in last_name:
+                last_name = last_name.replace("ä", "ae")
+            elif 'ü' in last_name:
+                last_name = last_name.replace("ü", "ue")
+            elif 'ö' in last_name:
+                last_name = last_name.replace("ö", "oe")
+            elif 'ß' in last_name:
+                last_name = last_name.replace("ß", "ss")
+            file.write(f'{id + 1},{first_name},{last_name},{fake.postcode()}\n')
+            id += 1
+
+if __name__ == '__main__':
+    menu()
